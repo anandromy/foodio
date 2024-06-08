fd1101002ecf86877a6a8b9b875542d57843c0af
anandromy <23f3003270@ds.study.iitm.ac.in>
2024-06-08 08:46:13 +0530

    update createRestaurant api request

--- src/api/myRestaurantApi.tsx
+++ src/api/myRestaurantApi.tsx
@@ -1,3 +1,4 @@
+import { Restaurant } from "@/types/user"
 import { useAuth0 } from "@auth0/auth0-react"
 import { useMutation } from "react-query"
 import { toast } from "sonner"
@@ -6,25 +7,29 @@ const api_base_url = import.meta.env.VITE_API_BASE_URL
 
 export const useCreateMyRestaurant = () => {
     const { getAccessTokenSilently } = useAuth0()
-    const createMyRestaurantRequest = async(createRestaurantData: FormData) => {
+    const createMyRestaurantRequest = async(createRestaurantData: FormData): Promise<Restaurant> => {
         const accessToken = await getAccessTokenSilently()
         const res = await fetch(`${api_base_url}/api/my/restaurant`, {
             method: "POST",
             headers: {
                 "Authorization" : `Bearer ${accessToken}`,
-                "Content-Type" : "application/json"
             },
             body: createRestaurantData
         })
         if(!res.ok){
             throw new Error("Failed to create restaurant")
         }
+
+        return res.json()
     }
 
-    const { mutateAsync: createRestaurant, error, isSuccess, isLoading, reset } = useMutation(createMyRestaurantRequest)
+    const { mutate: createRestaurant, error, isSuccess, isLoading, reset } = useMutation(createMyRestaurantRequest)
     if(error){
-        toast.error(error.toString())
+        toast.error("Unable to update restaurant")
         reset();
     }
-    return { createRestaurant, isLoading, isSuccess }
+    if(isSuccess){
+        toast.success("Restaurant Created!")
+    }
+    return { createRestaurant, isLoading }
 }
