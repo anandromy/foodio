24a6c971d95ec72eddaa18266692480396477949
anandromy <23f3003270@ds.study.iitm.ac.in>
2024-06-08 19:03:53 +0530

    loading spinner

--- src/auth/protectedRoutes.tsx
+++ src/auth/protectedRoutes.tsx
@@ -1,10 +1,15 @@
 import { useAuth0 } from "@auth0/auth0-react"
+import { LoaderIcon } from "lucide-react"
 import { Navigate, Outlet } from "react-router-dom"
 
 export const ProtectedRoute = () => {
     const { isAuthenticated, isLoading } = useAuth0()
     if(isLoading){
-        return null;
+        return (
+            <div className="min-h-screen flex items-center justify-center">
+                <LoaderIcon className="animate-spin" />
+            </div>
+        )
     }
 
     if(isAuthenticated){
